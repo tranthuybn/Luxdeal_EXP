c38e5a0c30f0804d9018616fde07a03b4ba705f1
ngocanc5 <anlengocc5@gmail.com>
2022-08-31 10:44:45 +0700

    update mock news

--- mock/NewsAndAdvertisement/newslist.ts
+++ mock/NewsAndAdvertisement/newslist.ts
@@ -1,6 +1,4 @@
 import Mock from 'mockjs'
-import { config } from '@/config/axios/config'
-
 interface News {
   id: Number
   title: string
@@ -30,26 +28,5 @@ for (let i = 0; i < count; i++) {
     })
   )
 }
-const { result_code } = config
-const timeout = 1000
 
-export default [
-  {
-    url: '/NewsList',
-    method: 'get',
-    timeout,
-    response: ({ query }) => {
-      const { pageIndex, pageSize } = query
-      const pageList = NewsList.filter(
-        (_, index) => index < pageSize * pageIndex && index >= pageSize * (pageIndex - 1)
-      )
-      return {
-        code: result_code,
-        data: {
-          total: NewsList.length,
-          list: pageList
-        }
-      }
-    }
-  }
-]
+export { NewsList }

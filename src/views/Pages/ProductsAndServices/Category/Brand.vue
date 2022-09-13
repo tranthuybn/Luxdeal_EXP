d03b12f52844047e42d20ef15b8a356b7e9971f4
Ngô Hà <ngoducha6@gmail.com>
2022-09-13 10:13:46 +0700

    pull and resolve conflig form  developer

--- src/views/Pages/ProductsAndServices/Category/Brand.vue
+++ src/views/Pages/ProductsAndServices/Category/Brand.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { getCategory } from '@/api/LibraryAndSetting'
+import { getCategories } from '@/api/LibraryAndSetting'
 import { brandCategories } from './CategoryManagement'
 import TableType01 from '../../Components/TableDataBase.vue'
 import { provide } from 'vue'
-import { PRODUCTS_AND_SERVICES_CATEGORY } from '@/utils/API.Variables'
-const params = { TypeName: PRODUCTS_AND_SERVICES_CATEGORY.BRAND }
+import { PRODUCTS_AND_SERVICES } from '@/utils/API.Variables'
+const params = { TypeName: PRODUCTS_AND_SERVICES[5].key }
 provide('parameters', {
   params
 })
 </script>
 <template>
-  <TableType01 :columns="brandCategories" :api="getCategory" :selection="false" />
+  <TableType01 :columns="brandCategories" :api="getCategories" :selection="false" />
 </template>

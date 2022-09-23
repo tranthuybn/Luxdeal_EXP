c732e7d1938b65fb5f6fba16ae5ac8812448bc89
Ngô Hà <ngoducha6@gmail.com>
2022-09-23 14:14:11 +0700

    add tabs list order and fix ui new order list and add new rental order list

--- src/utils/API_URL.ts
+++ src/utils/API_URL.ts
@@ -7,6 +7,7 @@ export const PRODUCTS_AND_SERVICES_API = {
   UPDATE_CATEGORY: '/api/v1/Category/updateCategory',
   DELETE_CATEGORY: '/api/v1/Categories/DeleteCategory',
   GET_PRODUCT: '/api/v1/Product/GetProduct',
+  GET_PRODUCTS: '/api/v1/Product/GetProductProperty',
   GET_SPA: '/api/v1/Spa/GetListOfSpaService',
   POST_SPA: '/api/v1/Spa/CreateANewSpaSevice',
   PUT_SPA: '/api/v1/Spa/UpdateSpaService',
@@ -14,5 +15,6 @@ export const PRODUCTS_AND_SERVICES_API = {
   GET_SPASERVICE: '/api/v1/Spa/GetSpaService'
 }
 export const CUSTOMER_API = {
-  GET_POTENTIAL_CUSTOMERS: '/api/v1/Customer/GetPotentialCustomers'
+  GET_POTENTIAL_CUSTOMERS: '/api/v1/Customer/GetPotentialCustomers',
+  ADD_POTENTIAL_CUSTOMER: '/api/v1/Customer/addPotentialCustomers'
 }

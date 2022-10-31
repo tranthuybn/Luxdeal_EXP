5f16055579e5b4a485008eaed9a7842462c6eb70
ndTruongkatz <ndtruong1520@gmail.com>
2022-10-31 17:34:44 +0700

    code api warehouse fix table

--- src/views/Pages/Warehouse/Warehouse.ts
+++ src/views/Pages/Warehouse/Warehouse.ts
@@ -127,3 +127,107 @@ export const wareHouse = [
     filters: filterIventory
   }
 ]
+
+export const wareHouseContainer = [
+  {
+    field: 'index',
+    label: t('reuse.index'),
+    type: 'index',
+    align: 'center'
+  },
+  {
+    field: 'productCode',
+    label: t('reuse.importExportTransferWarehouse'),
+    minWidth: '150',
+    filters: filterIventory
+  },
+  {
+    field: 'managementCode',
+    label: t('reuse.importExportTransferWarehouseCode'),
+    minWidth: '150',
+    filters: filterIventory
+  },
+  {
+    field: 'productName',
+    label: t('reuse.orderCode'),
+    minWidth: '150',
+    filters: filterIventory
+  },
+  {
+    field: 'description',
+    label: t('reuse.typeOfProduct'),
+    minWidth: '150',
+    filters: filterIventory
+  },
+  {
+    field: 'productName',
+    label: t('reuse.managementCode'),
+    minWidth: '150',
+    filters: filterIventory
+  },
+  {
+    field: 'characteristic',
+    label: t('reuse.productInformation'),
+    minWidth: '250'
+  },
+  {
+    field: 'accessory',
+    label: t('reuse.accessory'),
+    minWidth: '100'
+  },
+  {
+    field: 'singleEntryCode',
+    label: t('reuse.category'),
+    minWidth: '100',
+    filters: filterService
+  },
+
+  {
+    field: 'typeOfFirstEntry',
+    label: t('reuse.note'),
+    minWidth: '100'
+  },
+  {
+    field: 'typeOfTransfer',
+    label: t('reuse.quantity'),
+    minWidth: '100'
+  },
+  {
+    field: 'warehouseManagement',
+    label: t('reuse.dram'),
+    minWidth: '100'
+  },
+  {
+    field: 'amountImport',
+    label: t('reuse.warehouse'),
+    minWidth: '100',
+    filters: filterIventory
+  },
+  {
+    field: 'location',
+    label: t('reuse.createDateBill'),
+    minWidth: '100',
+    sortable: true
+  },
+
+  {
+    field: 'inventory',
+    label: t('customerList.petitioner'),
+    minWidth: '100',
+    sortable: true,
+    filters: filterIventory
+  },
+  {
+    field: 'dram',
+    label: t('reuse.subject'),
+    minWidth: '100',
+    filters: filterIventory
+  },
+
+  {
+    field: 'status',
+    label: t('reuse.status'),
+    minWidth: '100',
+    filters: filterIventory
+  }
+]

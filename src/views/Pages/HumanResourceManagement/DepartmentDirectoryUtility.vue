9b0e8b58a071019c60867575a17f9f348a391c69
cooftechdev <cooftechdev@gmail.com>
2022-12-15 09:28:47 +0700

    upgrade version

--- src/views/Pages/HumanResourceManagement/DepartmentDirectoryUtility.vue
+++ src/views/Pages/HumanResourceManagement/DepartmentDirectoryUtility.vue
@@ -14,7 +14,6 @@ const schema = reactive<FormSchema[]>([
   },
   {
     field: 'branchCode',
-    field: 'branchCode',
     label: t('reuse.branchCode'),
     component: 'Input',
     colProps: {
@@ -22,7 +21,6 @@ const schema = reactive<FormSchema[]>([
     }
   },
   {
-    field: 'branchName',
     field: 'branchName',
     label: t('reuse.branchName'),
     component: 'Input',
@@ -32,12 +30,10 @@ const schema = reactive<FormSchema[]>([
   },
   {
     field: 'statusAndFunction',
-    field: 'statusAndFunction',
     label: t('reuse.statusAndFunction'),
     component: 'Divider'
   },
   {
-    field: 'status',
     field: 'status',
     label: t('reuse.status'),
     component: 'Checkbox',
@@ -211,38 +207,7 @@ const router = useRouter()
 const currentRoute = String(router.currentRoute.value.params.backRoute)
 // const title = router.currentRoute.value.meta.title
 const tab = router.currentRoute.value.params.tab
-const id = Number(router.currentRoute.value.params.id)
 const type = String(router.currentRoute.value.params.type)
-
-// custom api form post
-type FormDataPost = {
-  MachiNhanh?: string
-  NameChiNhanh?: string
-  status?: string
-}
-const customPostDataBranch = (data) => {
-  const customData = {} as FormDataPost
-
-  customData.MachiNhanh = data.Machinhanh
-  customData.NameChiNhanh = data.TenChinhanh
-  customData.status = data.status
-
-  return customData
-}
-
-// custom api form edit
-type FormDataEdit = {
-  MachiNhanh?: string
-  NameChiNhanh?: string
-  status?: string
-}
-const customEditDataBranch = (data) => {
-  const getData = {} as FormDataEdit
-
-  getData.MachiNhanh = data.branchCode
-  getData.NameChiNhanh = data.branchName
-  getData.status = data.status
-
 const postData = (data) => {
   console.log('data: ', data)
   const payload = {

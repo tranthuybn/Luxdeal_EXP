8aa4e3c9146d919802199f32001ca98c84e301fd
Dũng Vũ <vmd101297@gmail.com>
2023-03-06 15:01:03 +0700

    sua ngon ngu

--- src/views/Pages/Business/OrderManagement/addNewRentalOrdersUtility.vue
+++ src/views/Pages/Business/OrderManagement/addNewRentalOrdersUtility.vue
@@ -4907,7 +4907,7 @@ const disabledPhieu = ref(false)
           </el-table>
           <div class="flex justify-end">
             <div class="w-[145px] text-right">
-              <p class="text-black font-bold dark:text-white">{{t('formDemo.totalPayment')}}</p>
+              <p class="text-black font-bold dark:text-white">{{t('reuse.totalPayment')}}</p>
             </div>
             <div class="w-[145px] text-right">
               <p class="pr-2 text-black font-bold dark:text-white">{{

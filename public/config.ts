58b75b2bef8f23234c281a9fc9896c2433f0d5f8
huynt.cftshop@gmail.com <huynt.cftshop@gmail.com>
2023-04-04 18:36:18 +0700

    huy commit print

--- public/config.ts
+++ public/config.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 //export const ERP_DOMAIN = 'https://dev-luxdeal-api.cftsoft.com/'
 export const ERP_DOMAIN = 'http://103.81.87.134:1223/'
 export const ERP_DOMAIN_LOGIN = 'https://dev-login.cftsoft.com/'
 export const ERP_DOMAIN_SOCKET = 'ws://103.81.87.134:8383/'
 export const ERP_DOMAIN_CHATS = 'http://103.81.87.134:8017/'
 // export const ERP_DOMAIN = 'https://localhost:44356/'
-=======
- export const ERP_DOMAIN = 'https://dev-luxdeal-api.cftsoft.com/'
-//  export const ERP_DOMAIN = 'http://103.81.87.134:1223/'
-export const ERP_DOMAIN_LOGIN = 'https://dev-login.cftsoft.com/'
-export const ERP_DOMAIN_SOCKET = 'ws://103.81.87.134:8383/'
-export const ERP_DOMAIN_CHATS = 'http://103.81.87.134:8017/'
- //export const ERP_DOMAIN = 'https://localhost:44356/'
->>>>>>> 770bdb92b300aeaf695bb4bc01d86de6158e8e3f

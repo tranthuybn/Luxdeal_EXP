f0a610a803146fd31e0b6bd8483551186003dcff
tranthuybn0611 <thuytt.cftshop@gmail.com>
2023-04-26 06:38:06 +0700

    fix bug

--- src/views/Pages/Components/formPrint/src/paymentOrderPrint.vue
+++ src/views/Pages/Components/formPrint/src/paymentOrderPrint.vue
@@ -2,6 +2,7 @@
 import { ElDivider, ElTable, ElTableColumn } from 'element-plus'
 import { changeMoney } from '@/utils/tsxHelper'
 import { useI18n } from '@/hooks/web/useI18n'
+import { computed, onBeforeMount, reactive } from 'vue'
 
 const { t } = useI18n()
 
@@ -15,13 +16,45 @@ const props = defineProps({
     default: () => {}
   }
 })
+const dataTableDefault: any = reactive([])
+onBeforeMount(() => {
+  for (let i = 0; i < 10; i++) {
+    dataTableDefault.push({
+        numberVouchers: '',
+        dayVouchers: '',
+        spentFor: '',
+        quantity: '',
+        unitPrice: '',
+        finalPrice: '',
+        note: ''
+    })
+  } 
+})
+
+const dataTable = computed(() => {
+  if(props.dataSent?.detailedListExpenses.length > 0) return props.dataSent?.detailedListExpenses
+  return dataTableDefault
+})
+
+const checkUnitPrice = computed(() => {
+  const price = Number(props.dataSent?.detailedListExpenses?.unitPrice)
+  if(price) return currencyFormatter.format(price)
+  return ''
+})
+const checkFinalPrice = computed(() => {
+  const price = Number(props.dataSent?.detailedListExpenses?.unitPrice)
+  const quantity = Number(props.dataSent?.detailedListExpenses?.quantity)
+  if(price) return currencyFormatter.format(price * quantity)
+  return ''
+})
+// console.log('dataTableDefault', dataTableDefault)
 console.log('dataEdit', props.dataEdit)
 console.log('dataSent', props.dataSent)
 const currencyFormatter = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' })
 </script>
 
 <template>
-  <div class="w-full h-full p-4">
+  <div class="w-full h-full p-4 pt-7">
     <div class="flex" style="width: 100%; font-size: 10px;">
       <div class="basis-3/12">
         <img src="@/assets/imgs/images.png" />
@@ -87,30 +120,30 @@ const currencyFormatter = new Intl.NumberFormat('vi-VN', { style: 'currency', cu
       </div>
     </div>
     <div>
-      <div class="font-bold text-xs">{{ t('formDemo.detailedlistofexpenses') }}</div>
-      <el-table size="small" ref="singleTableRef" :data="dataSent ? dataSent.detailedListExpenses : []"  border>
+      <div class="font-bold text-xs mb-2">{{ t('formDemo.detailedlistofexpenses') }}</div>
+      <el-table size="small" ref="singleTableRef" :data="dataTable"  border>
         <el-table-column label="Chứng từ" align="center">
-          <el-table-column prop="STT" type="index" label="STT"  />
+          <el-table-column align="center" type="index" label="STT"  />
           <el-table-column prop="numberVouchers" label="Số" />
           <el-table-column prop="dayVouchers" label="Ngày"  />
         </el-table-column>
         <el-table-column prop="spentFor" width="90px" :label="t('formDemo.spendFor')" />
         <el-table-column prop="quantity" width="40px" :label="t('formDemo.sl')"  />
         <el-table-column prop="unitPrice"  width="50px" :label="t('reuse.unitPrice')">
-          <template #default="data">
-            <div class="text-right">{{ currencyFormatter.format(data.row.unitPrice) }}</div>
+          <template #default>
+            <div class="text-right">{{ checkUnitPrice }}</div>
           </template>
         </el-table-column>
         <el-table-column prop="finalPrice" width="60px" :label="t('reuse.amountMoneyVnd')">
-          <template #default="data">
-            <div class="text-right">{{ currencyFormatter.format(data.row.quantity * data.row.unitPrice) }}</div>
+          <template #default>
+            <div class="text-right">{{ checkFinalPrice }}</div>
           </template>
         </el-table-column>
         <el-table-column prop="note" :label="t('reuse.note')"  />
       </el-table>
     </div>
   
-    <div class="flex h-[16vh] justify-between text-center">
+    <div class="flex h-[16vh] justify-between text-center mt-4">
       <div class="text-xs font-medium border-b basis-3/10">
         <div> Giám đốc </div>
       </div>
